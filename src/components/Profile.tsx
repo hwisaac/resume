7ae0f513d0cc294bf9567951e2dfe3b2ba2e967a
hwisaac <54179672+hwisaac@users.noreply.github.com>
2025-03-10 07:54:44 +0900

    fix: 프로필 컴포넌트 및 인덱스 HTML 경로 수정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,20 +8,26 @@ type Props = {}
 export default function Profile({}: Props) {
   return (
     <div className='flex my-4 px-4'>
-        <div className='size-[140px] rounded-full bg-slate-100 mr-8' />
-        <ul >
-            <li className='font-semibold text-xl mb-1'>황이삭</li>
-            <li>Web Developer</li>
-            <li>Seoul, Korea</li>
-            <li className='my-2'>
-            <a className='flex items-center gap-2' href="https://github.com/hwisaac" target='_blank' rel='noreferrer'><FaGithub  size={20} className='relative top-[0.5px]' /> https://githubm.com/hwisaac</a>
-
-            </li>
-            <li className='flex gap-2 items-center'>
-            <AiOutlineMail size={20} className='relative top-[0.5px]' />
-             wh2ssac@naver.com
-            </li>
-        </ul>
+      <div className='size-[140px] rounded-full bg-slate-100 mr-8' />
+      <ul>
+        <li className='font-semibold text-xl mb-1'>황이삭</li>
+        <li>Web Developer</li>
+        <li>Seoul, Korea</li>
+        <li className='my-2'>
+          <a
+            className='flex items-center gap-2'
+            href='https://github.com/hwisaac'
+            target='_blank'
+            rel='noreferrer'>
+            <FaGithub size={20} className='relative top-[0.5px]' />{' '}
+            https://github.com/hwisaac
+          </a>
+        </li>
+        <li className='flex gap-2 items-center'>
+          <AiOutlineMail size={20} className='relative top-[0.5px]' />
+          wh2ssac@naver.com
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
